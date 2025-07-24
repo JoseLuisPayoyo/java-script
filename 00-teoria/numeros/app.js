f55cd145331476197ea3fe89174321b8fda3250e
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-24 15:59:09 +0200

    fin numeros

--- 00-teoria/numeros/app.js
+++ 00-teoria/numeros/app.js
@@ -13,5 +13,21 @@ console.log(Math.sqrt(4))
 console.log(Math.abs(-400))
 console.log(Math.pow(8, 2))
 
-//Orden de las operaciones
+//Orden de las operacion
+console.log(20 + 30 * 2)
+console.log((20 + 30) * 2)
+
+//convertir Strings a numeros
+const n1 = "20"
+const n1_2 = 20
+const n2 = "20.2"
+const n3 = "Uno"
+const n4 = 20
+
+console.log(Number.parseInt(n1))
+console.log(Number.parseFloat(n2))
+
+//revisar si es entero
+console.log(Number.isInteger(n1))
+console.log(Number.isInteger(n1_2))
 
