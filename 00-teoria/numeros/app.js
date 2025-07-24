8574440374339e4074d7c8434553098932f0a375
Jose Luis Rodriguez <josepayoyo2003@gmail.com>
2025-07-24 15:54:04 +0200

    Objeto Math

--- 00-teoria/numeros/app.js
+++ 00-teoria/numeros/app.js
@@ -1,1 +1,17 @@
-console.log("Hola");
+const numero = 30;
+const numero1 = 30.9;
+
+console.log(numero);
+console.log(numero1);
+
+// Objeto Math
+console.log(Math.floor(2.4))
+console.log(Math.PI)
+console.log(Math.round(2.8))
+console.log(Math.ceil(2.1))
+console.log(Math.sqrt(4))
+console.log(Math.abs(-400))
+console.log(Math.pow(8, 2))
+
+//Orden de las operaciones
+
